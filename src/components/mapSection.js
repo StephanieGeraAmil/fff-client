1f9c358c93dac1c591585c30c5bf70c8aecb8562
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2022-02-23 12:16:43 -0300

    selecting markers and showing info about them

--- src/components/mapSection.js
+++ src/components/mapSection.js
@@ -1,6 +1,7 @@
 import React, {useCallback, useState} from 'react'
-import { GoogleMap,Marker,useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap,Marker,InfoWindow,useJsApiLoader } from '@react-google-maps/api';
 import mapStyles from "./mapStyles";
+import { formatRelative } from "date-fns";
 
 //const libraries=['places']
 
@@ -24,6 +25,7 @@ const options = {
 export const MapSection = () => {
 
   const [markers, setMarkers] = useState([]);
+  const [selected, setSelected] = React.useState(null);
 
   const onMapClick = useCallback((e) => {
     setMarkers((current) => [
@@ -52,26 +54,41 @@ export const MapSection = () => {
          onClick={onMapClick}
        
       >
-      {/* {markers.map( (marker)=> ( <Marker  
-            key={`${marker.lat}*${marker.lng}*${marker.time}`}
-            position={{ lat: marker.lat, lng: marker.lng }}
-            /> ))}; */}
-
+    
        {markers.map((marker) => (
           <Marker
             key={`${marker.lat}-${marker.lng}`}
             position={{ lat: marker.lat, lng: marker.lng }}
-            // onClick={() => {
-            //   setSelected(marker);
-            // }}
-            // icon={{
-            //   url: `/bear.svg`,
-            //   origin: new window.google.maps.Point(0, 0),
-            //   anchor: new window.google.maps.Point(15, 15),
-            //   scaledSize: new window.google.maps.Size(30, 30),
-            // }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+            icon={{
+              url: `/bible.png`,
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(35, 40),
+              scaledSize: new window.google.maps.Size(70, 80),
+            }}
           />
         ))}
+        {selected ? (
+            <InfoWindow
+              position={{ lat: selected.lat, lng: selected.lng }}
+              onCloseClick={() => {
+                setSelected(null);
+              }}
+            >
+              <div>
+                <h2>
+                Event
+                </h2>
+                 <p>Latitude {selected.lat}</p>
+                  <p>Longitude {selected.lng}</p>
+                <p>Created {formatRelative(selected.time, new Date())}</p>
+              </div>
+            </InfoWindow>
+          ) : null}
+
+
       </GoogleMap>
   ) : <></>
 }
