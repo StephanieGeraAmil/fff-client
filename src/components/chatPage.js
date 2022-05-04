9ceea23c71da9ee0df90a9923750d5db993eab57
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2022-05-04 17:47:31 -0300

    bug on messages refresh fixed

--- src/components/chatPage.js
+++ src/components/chatPage.js
@@ -1,67 +1,37 @@
 import React ,{useEffect, useState}from 'react'
-import { useDispatch,useSelector } from 'react-redux';
+import {useSelector } from 'react-redux';
 import {Message} from './message';
-import {setMessages, addMessage, clearMessages}  from '../actions/messageActions';
 import Button from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
 import Stack from 'react-bootstrap/Stack';
-
 import Form from 'react-bootstrap/Form';
-
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import {io} from 'socket.io-client';
 
+export const ChatPage = ({ socket}) => {
 
-export const ChatPage = ({selected_chat}) => {
-  const id=selected_chat._id;
-  const dispatch=useDispatch();
-
+  const selectedChat=useSelector((state)=>state.current.chat); 
   const messagesOfChat = useSelector((state)=>state.messages)
   const currentUser = useSelector((state)=>state.current.user)
   const [loaded, setLoaded]= useState(false)
   const [msg, setMsg]= useState("")
-  const [skt, setSkt]= useState({})
+ 
 
   const handleKeyPress=(e)=>{
     if(e.key === 'Enter'){
        handleSubmit(e)
      }  
-     
   }
   const handleSubmit=(e)=>{
      e.preventDefault();
-    skt.emit("message-sent", {   sender:currentUser,
+    socket.emit("message-sent", {   sender:currentUser,
                                   content:msg},
-                                  id);
-    setMsg(""); 
-    console.log("messaged emmited")
-    console.log(skt)
-
+                                  selectedChat._id);
+    setMsg("");
   }
-  useEffect(()=>{
-
-      const socket=io(process.env.REACT_APP_BACKEND_URL);
-      setSkt(socket);
-    
-      socket.on("connect",()=>{ socket.emit("get-last-100-messages",id); }); 
-      socket.on("last-100-messgaes-from-chat",(data)=> { dispatch(setMessages(data))});
-      socket.on ("new-message",  (message)  =>{ dispatch(addMessage(message)); });
-     
-      
-      return()=>{
-        socket.disconnect()
-        setSkt({});
-        dispatch(clearMessages());
-       }
-      
-  },[])
 
   useEffect(()=>{
     setLoaded(true);
@@ -69,7 +39,7 @@ export const ChatPage = ({selected_chat}) => {
   
   return ( <>
      {loaded && <Container className='chat'>
-          <h4 className='chat_title'>{selected_chat.title}</h4>
+          <h4 className='chat_title'>{selectedChat.title}</h4>
           <ListGroup className='chat_messages'>
             {messagesOfChat.map((item) => {
                 return (
--- src/components/chatSection.js
+++ src/components/chatSection.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect , useState} from 'react'
 import {ChatItem} from './chatItem';
 import {ChatPage} from './chatPage';
 import {useAuth0 } from "@auth0/auth0-react";
@@ -7,50 +7,52 @@ import {getChats} from '../actions/chatActions.js';
 import {clearSelectedChat} from '../actions/globalStateActions.js';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
-
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import {io} from 'socket.io-client';
+import {setMessages, addMessage, clearMessages}  from '../actions/messageActions';
 
 export const ChatSection = () => {
-    const { isAuthenticated} = useAuth0();
+    // const { isAuthenticated} = useAuth0();
     const chats=useSelector((state)=>state.chats); 
     const selectedChat=useSelector((state)=>state.current.chat); 
+    const [skt, setSkt]= useState({})
     const dispatch= useDispatch();
+
     useEffect(()=>{
       dispatch(getChats());
-      return()=>{
+      const socket=io(process.env.REACT_APP_BACKEND_URL);
+      socket.on("last-100-messgaes-from-chat",(data)=> { dispatch(setMessages(data));});
+      socket.on ("new-message",  (message)  =>{ dispatch(addMessage(message)); });
+
+      setSkt(socket);
       
+      return()=>{
+        socket.disconnect()
         dispatch(clearSelectedChat());
+        dispatch(clearMessages());
        }
       
     },[])
 
   return (<>
-   {/* <div className='auxdiv'>
-
-   </div> */}
-   <div className='chat_section'>
-        <Container className='chat_list'>
-              <ListGroup className='mh-100' >
-                    {chats.map((item) => {
-                        return (
-                            <ListGroupItem  key={item._id}>
-                                <ChatItem key={item._id} chat={item}/>
-                            </ListGroupItem>
-                        )
-                    })}
-                </ListGroup>
-          </Container>
-          <div className='chatView'>
-              {selectedChat&&<ChatPage selected_chat={selectedChat}/>}
-        </div>
-     
-     
-       {/* later on an option to create a chat room will be added here */}
-      </div>
-      </>
+              <div className='chat_section'>
+                    <Container className='chat_list'>
+                          <ListGroup className='mh-100' >
+                                {chats.map((item) => {
+                                    return (
+                                        <ListGroupItem  key={item._id}>
+                                            <ChatItem key={item._id} chat={item} socket={skt}/>
+                                        </ListGroupItem>
+                                    )
+                                })}
+                            </ListGroup>
+                      </Container>
+                      <div className='chatView'>
+                          {selectedChat&&<ChatPage socket={skt}/>}
+                    </div>
+                  {/* later on an option to create a chat room will be added here */}
+               </div>
+          </>
   )
 }
