76d709dd5106d009c44dc4c560ce0451a4257f64
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2022-05-06 20:05:44 -0300

    fixing refresh on dates bug

--- src/components/addEventForm.js
+++ src/components/addEventForm.js
@@ -6,11 +6,78 @@ import Form from 'react-bootstrap/Form';
 import CloseButton from 'react-bootstrap/CloseButton';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Stack from 'react-bootstrap/esm/Stack';
 
 export const AddEventForm = ({socket}) => {
   const form = useSelector((state) =>(state.current.form ? state.current.form :null));
   const userLogged = useSelector((state) =>(state.current.user ? state.current.user :null));
   const dispatch= useDispatch();
+  const date= new Date();
+  const [day, setDay]=useState(date.getDate().toString());
+  const [month, setMonth]=useState((date.getMonth()+1).toString());
+  const [year, setYear]=useState(date.getFullYear().toString());
+  const [yearValues, setYearValues]=useState([]);
+  const [dayValues, setDayValues]=useState([]);
+
+   const generateArrayOfDays=()=> {
+    const days=[]
+    let dayNum;
+
+    if (
+      (month === "0") |
+      (month === "2") |
+      (month === "4") |
+      (month === "6") |
+      (month === "7") |
+      (month ==="9") |
+      (month === "11")
+    ) {
+      dayNum = 31;
+    } else if (
+      (month === "3") |
+      (month === "5") |
+      (month === "8") |
+      (month ==="10")
+    ) {
+      dayNum = 30;
+    } else {
+      
+      const isLeap = new Date(year, 1, 29).getMonth() == 1;
+      isLeap ? (dayNum = 29) : (dayNum = 28);
+    }
+    for (let i = 1; i <= dayNum; i++) {
+       days.push(i)
+    }
+    setDayValues(days)
+  }
+
+
+  const monthVaules=[ {value:0, name:"January"},
+                      {value:1, name:"February"},
+                      {value:2, name:"March"},
+                      {value:3, name:"April"},
+                      {value:4, name:"May"},
+                      {value:5, name:"June"},
+                      {value:6, name:"July"},
+                      {value:7, name:"August"},
+                      {value:8, name:"September"},
+                      {value:9, name:"October"},
+                      {value:10, name:"November"},
+                      {value:11, name:"December"},
+  ];
+
+  function generateArrayOfYears() {
+        const max = new Date().getFullYear()
+        const min = max - 109
+        const years=[]
+        for (let i = max; i >= min; i--) {
+          years.push(i)
+        }
+        setYearValues(years);
+    
+  }
+
+ 
  
   const typesAvaiable=[
                   {name:"Bible Study", img: `/bible.png`, id:1},
@@ -29,9 +96,16 @@ export const AddEventForm = ({socket}) => {
          img:"."+typesAvaiable[0].img,
          lat:form.positionSelected.lat, 
          lng:form.positionSelected.lng,
-         creator: userLogged._id
+         creator: userLogged._id,
+   
     });
-
+   useEffect(()=>{
+      generateArrayOfDays();
+  },[month,year])
+    useEffect(()=>{
+       generateArrayOfDays();
+      generateArrayOfYears();
+        },[])
 
   const handleSubmit=(e)=>{
       e.preventDefault();
@@ -40,7 +114,12 @@ export const AddEventForm = ({socket}) => {
       dispatch(unsetForm());
     };
 
-  
+    const setDate=()=>{
+   
+      const dateToStore = new Date(year, month, day);
+      setEventData({...eventData, end_date:(dateToStore)})
+   
+  }
 
   return (
   <>
@@ -56,6 +135,20 @@ export const AddEventForm = ({socket}) => {
               { typesAvaiable.map((item)=> {return( <option key={item.id}>{item.name}</option>)})}
               
               </Form.Select>
+              <Stack direction="horizontal" gap={3} className='mb-3' >
+              <Form.Select  onChange={(e)=>{setDay(e.target.value); setDate() ;} } defaultValue={day} >
+              { dayValues.map((day)=> {return( <option key={day}>{day}</option>)})}
+                  
+              
+              </Form.Select>
+              <Form.Select   onChange={(e)=>{setMonth(e.target.value); setDate(); }} defaultValue={month}>
+                    { monthVaules.map((m)=> {return( <option key={m.value} value={m.value}>{m.name}</option>)})}
+                  
+              </Form.Select>
+              <Form.Select  onChange={(e)=>{setYear(e.target.value); setDate(); }} defaultValue={year} >
+                      { yearValues.map((year)=> {return( <option key={year}>{year}</option>)})}
+              </Form.Select>
+              </Stack>
 
                <Button variant="secondary" size="lg" className='mb-3' onClick={(e)=>handleSubmit(e)}>Add Event</Button>
 
