20a26c29e9ceefc619112c8aa5f97c4aadc37e2f
Stephanie <stephanie.gera.amil@gmail.com>
2023-02-11 02:15:25 -0300

    calling the updateUser method

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -16,7 +16,10 @@ export const createUser=(user)=>async(dispatch)=>{
 }
 export const updateUser=(updatedUser)=>async(dispatch)=>{
     try {
-        const action={type:actions.UPDATE_USER, payload:updatedUser};
+       
+        const {data}=await api.updateUser(updatedUser);
+         console.log(data)
+        const action={type:actions.UPDATE_USER, payload:data};
         dispatch(action);
         
     } catch (error) {
