a6611ca9c115aa53a57149e09ccecbc6bf68f698
Stephanie <stephanie.gera.amil@gmail.com>
2023-02-11 12:23:28 -0300

    deleting everything related to chats and it's messages because I no longer need that

--- src/reducers/globalStateReducer.js
+++ src/reducers/globalStateReducer.js
@@ -1,5 +1,5 @@
 import * as actions from '../actionTypes'
-export default (current={user:null,form:null, chat:null}, action)=>{
+export default (current={user:null,form:null}, action)=>{
     switch(action.type){
         case actions.SET_FORM:
         return {...current,form: action.payload};
@@ -13,14 +13,6 @@ export default (current={user:null,form:null, chat:null}, action)=>{
            
         case actions.UNSET_USER:
         return {...current,user: null};
-
-        case actions.SET_SELECTED_CHAT:
-        return {...current,chat: action.payload};
-         
-        case actions.CLEAR_SELECTED_CHAT:
-        return {...current,chat: null};
-
-
 
         default:
         return current;
