42dbf918b9d5a65ecab67022bfcf3570d9d8f430
Stephanie <stephanie.gera.amil@gmail.com>
2023-02-11 13:49:08 -0300

    deleting empty spaces and console logs

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,42 +1,31 @@
 import * as actions from '../actionTypes'
 import * as api from '../api.js';
 
-
-
 export const createUser=(user)=>async(dispatch)=>{
-  
     try {
         const {data}=await api.createUser(user);
         const action={type:actions.CREATE_USER, payload:data};
         dispatch(action);
-
     } catch (error) {
         console.log(error);
     }
 }
 export const updateUser=(updatedUser)=>async(dispatch)=>{
     try {
-       
         const {data}=await api.updateUser(updatedUser);
-         console.log(data)
         const action={type:actions.UPDATE_USER, payload:data};
-        dispatch(action);
-        
+        dispatch(action);   
     } catch (error) {
         console.log(error);
     }
 }
 export const deleteUser=(user_id)=>async(dispatch)=>{
     try {
-    
         const action={type: actions.DELETE_USER,payload:user_id};
         dispatch(action);
-        
     } catch (error) {
         console.log(error);
     }
-
-    
 }
 
 export const getUsers=()=>async(dispatch)=>{
