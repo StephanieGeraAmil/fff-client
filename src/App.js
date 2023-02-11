413a285462135f051c8d9767352605c36c85c533
Stephanie <stephanie.gera.amil@gmail.com>
2023-02-11 02:14:45 -0300

    formatting

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './styles/styles.css';
 import React, { useEffect, useState } from 'react'
 import {MapSection} from './components/mapSection';
 
-function App() {
+const App=()=> {
      return (
     <div className="App">
       <MapSection/>
      </div>
   );
 }
+
 export default App;
+
--- src/actions/globalStateActions.js
+++ src/actions/globalStateActions.js
@@ -1,53 +1,53 @@
-import * as actions from '../actionTypes'
-import * as api from '../api.js';
+import * as actions from "../actionTypes";
+import * as api from "../api.js";
 
+export const setForm = (form) => async (dispatch) => {
+  try {
+    console.log(form);
+    const action = { type: actions.SET_FORM, payload: form };
+    dispatch(action);
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const unsetForm = () => async (dispatch) => {
+  try {
+    const action = { type: actions.UNSET_FORM };
+    dispatch(action);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const setForm=(form)=>async(dispatch)=>{  
-    try {
-        const action={type:actions.SET_FORM, payload:form};
-        dispatch(action);
-    } catch (error) {
-        console.log(error);
+export const setUser = (user) => async (dispatch) => {
+  try {
+    const { data } = await api.findUserByEmail(user.email);
+
+    if (!data) {
+      const action = { type: actions.SET_FORM, payload: { type: "AddUser" } };
+      dispatch(action);
+    } else {
+      const action = { type: actions.SET_USER, payload: data.message };
+      dispatch(action);
     }
-}
-export const unsetForm=()=>async(dispatch)=>{  
-    try {
-        const action={type:actions.UNSET_FORM};
-        dispatch(action);
-    } catch (error) {
-        console.log(error);
-    }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const setUser=(user)=>async(dispatch)=>{    
-    try { 
-        const {data}=await api.findUserByEmail(user.email); 
-        if(!data){
-            const action={type:actions.SET_FORM, payload:{type:"AddUser"}};
-            dispatch(action);
-
-        }
-        const action={type:actions.SET_USER, payload:data};
-        dispatch(action);  
-    } catch (error) {
-        console.log(error); 
-    }
-}
-
-export const setSelectedChat=(form)=>async(dispatch)=>{  
-    try {
-        const action={type:actions.SET_SELECTED_CHAT, payload:form};
-        dispatch(action);
-    } catch (error) {
-        console.log(error);
-    }
-}
-export const clearSelectedChat=()=>async(dispatch)=>{  
-    try {
-        const action={type:actions.CLEAR_SELECTED_CHAT};
-        dispatch(action);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
+export const setSelectedChat = (form) => async (dispatch) => {
+  try {
+    const action = { type: actions.SET_SELECTED_CHAT, payload: form };
+    dispatch(action);
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const clearSelectedChat = () => async (dispatch) => {
+  try {
+    const action = { type: actions.CLEAR_SELECTED_CHAT };
+    dispatch(action);
+  } catch (error) {
+    console.log(error);
+  }
+};
