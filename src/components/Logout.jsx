08b7f417ad86506c43e8939b39f5a9c8c224d155
Stephanie <stephanie.gera.amil@gmail.com>
2023-03-07 13:49:20 -0300

    changing file extensions and formatting

---
+++ src/components/Logout.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+export const Logout = () => {
+  const { logout } = useAuth0();
+  return (
+    <button
+      className="top_link logout_button"
+      onClick={() => {
+        logout({ returnTo: window.location.origin });
+      }}
+    ></button>
+  );
+};
