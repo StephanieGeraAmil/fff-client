4d6c31e370810a9f1ff1cae974f6f03eb68cf88e
Stephanie <stephanie.gera.amil@gmail.com>
2023-03-23 16:32:01 -0300

    renewing the form Event, adding some fields and changing looks

--- src/components/forms/EventForm.jsx
+++ src/components/forms/EventForm.jsx
@@ -11,7 +11,20 @@ import {
   Select,
   MenuItem,
   InputLabel,
+  RadioGroup,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  Slider,
+  Grid,
+  Switch,
+  Stack,
+  Container,
+  Box,
 } from "@mui/material";
+import Woman2OutlinedIcon from "@mui/icons-material/Woman2Outlined";
+import WcOutlinedIcon from "@mui/icons-material/WcOutlined";
+import Man2OutlinedIcon from "@mui/icons-material/Man2Outlined";
 import { DatePicker } from "@mui/x-date-pickers";
 import dayjs from "dayjs";
 
@@ -28,9 +41,11 @@ export const EventForm = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [date, setDate] = useState("");
+  // const [eventDate, setEventDate] = useState("");
   const [eventType, setEventType] = useState("");
   const [targetGender, setTargetGender] = useState("");
-  const [targetAgeRange, setTargetAgeRange] = useState("");
+  const [repetition, setRepetition] = useState(false);
+  const [targetAgeRange, setTargetAgeRange] = useState([20, 37]);
 
   const typesAvaiable = [
     { name: "Bible Study", img: `/bible.png`, id: 1 },
@@ -42,6 +57,60 @@ export const EventForm = () => {
     { name: "Pray", img: `/pray.png`, id: 7 },
   ];
 
+  const minAgeRange = 5;
+  const handleChange = (event, newValue, activeThumb) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (activeThumb === 0) {
+      setTargetAgeRange([
+        Math.min(newValue[0], targetAgeRange[1] - minAgeRange),
+        targetAgeRange[1],
+      ]);
+    } else {
+      setTargetAgeRange([
+        targetAgeRange[0],
+        Math.max(newValue[1], targetAgeRange[0] + minAgeRange),
+      ]);
+    }
+  };
+  const handleRepeatChange = (event) => {
+    setRepetition(event.target.checked);
+  };
+  const marks = [
+    {
+      value: 10,
+    },
+    {
+      value: 20,
+      label: "20",
+    },
+    {
+      value: 30,
+    },
+    {
+      value: 40,
+      label: "40",
+    },
+    {
+      value: 50,
+    },
+    {
+      value: 60,
+      label: "60",
+    },
+    {
+      value: 70,
+    },
+    {
+      value: 80,
+      label: "80",
+    },
+    {
+      value: 90,
+    },
+  ];
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -53,7 +122,7 @@ export const EventForm = () => {
       type: typeOfEvent[0].name,
       img: "." + typeOfEvent[0].img,
       creator: userLogged.id,
-      expirationDate: date,
+      eventDate: date,
       targetGender: targetGender,
       targetAgeRange: targetAgeRange,
     };
@@ -75,9 +144,11 @@ export const EventForm = () => {
     if (form.type === "EditEvent") {
       //display the values already saved on the event
       if (form.event) {
-        if (form.event.expirationDate) {
-          const expirationDate = new Date(form.event.expirationDate);
-          setDate(dayjs(expirationDate.toISOString().slice(0, 10)));
+        if (form.event.date) {
+          const date = new Date(form.event.date);
+          setDate(dayjs(date.toISOString().slice(0, 10)));
+        } else {
+          setRepetition(false);
         }
         if (form.event.title) {
           setTitle(form.event.title);
@@ -92,7 +163,6 @@ export const EventForm = () => {
           setTargetGender(form.event.targetGender);
         }
         if (form.event.type) {
-          console.log(form.event.type);
           const typeOfEvent = typesAvaiable.filter(
             (type) => type.name === form.event.type
           );
@@ -104,92 +174,197 @@ export const EventForm = () => {
 
   return (
     <>
-      <Typography variant="h3" gutterBottom>
+      <Typography variant="h2" gutterBottom>
         New Event
       </Typography>
-      <FormGroup>
-        <FormControl sx={{ m: 1 }}>
-          <TextField
-            label="Title"
-            variant="outlined"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </FormControl>
-        <FormControl sx={{ m: 1 }}>
-          <TextField
-            label="Description"
-            variant="outlined"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </FormControl>
-        <FormControl sx={{ m: 1 }}>
-          <DatePicker
-            label="until"
-            value={date}
-            onChange={(newValue) => setDate(newValue)}
-          />
-        </FormControl>
-        <FormControl sx={{ m: 1 }}>
-          <InputLabel id="type-of-event">Type of Event</InputLabel>
-          <Select
-            labelId="type-of-event"
-            id="type-select"
-            label="Event type"
-            value={eventType}
-            onChange={(e) => setEventType(e.target.value)}
-          >
-            {typesAvaiable.map((type) => {
-              return (
-                <MenuItem key={type.id} value={type.id}>
-                  {type.name}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-        <FormControl sx={{ m: 1 }}>
-          <InputLabel id="gender-select-label">Target Gender</InputLabel>
-          <Select
-            labelId="gender-select-label"
-            id="gender-select"
-            label="Target Gender"
-            value={targetGender}
-            onChange={(e) => setTargetGender(e.target.value)}
-          >
-            <MenuItem value={"all"}>All</MenuItem>
-            <MenuItem value={"female"}>Female</MenuItem>
-            <MenuItem value={"male"}>Male</MenuItem>
-          </Select>
-        </FormControl>
 
-        <FormControl sx={{ m: 1 }}>
-          <InputLabel id="age-select-label">Target Age Range</InputLabel>
-          <Select
-            labelId="age-select-label"
-            id="age-select"
-            label="Target Age Range"
-            value={targetAgeRange}
-            onChange={(e) => setTargetAgeRange(e.target.value)}
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <FormControl sx={{ width: "100%" }}>
+            <TextField
+              label="Title"
+              variant="standard"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl sx={{ width: "100%" }}>
+            <TextField
+              multiline
+              maxRows={3}
+              label="Description"
+              variant="standard"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl variant="standard" sx={{ width: "100%" }}>
+            <InputLabel id="type-of-event">Type of Event</InputLabel>
+            <Select
+              labelId="type-of-event"
+              id="type-select"
+              label="Event type"
+              value={eventType}
+              onChange={(e) => setEventType(e.target.value)}
+            >
+              {typesAvaiable.map((type) => {
+                return (
+                  <MenuItem key={type.id} value={type.id}>
+                    {type.name}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}></Grid>
+        <Grid item xs={12}>
+          <Container
+            sx={{
+              width: "90%",
+              m: "auto",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              border: "0.5px solid white",
+              borderRadius: "10px",
+            }}
           >
-            <MenuItem value={"all"}>All</MenuItem>
-            <MenuItem value={"minor"}> less than 18</MenuItem>
-            <MenuItem value={"young"}>Between 18 and 25</MenuItem>
-            <MenuItem value={"adult "}>Between 26 and 40</MenuItem>
-            <MenuItem value={"mature"}>Between 41 and 60</MenuItem>
-            <MenuItem value={"older"}>More than 60</MenuItem>
-          </Select>
-        </FormControl>
+            <Typography
+              variant="subtitle2"
+              gutterBottom
+              sx={{ fontWeight: 100 }}
+            >
+              Who is it for?
+            </Typography>
+            <Grid container xs={12}>
+              <Grid item xs={2} sx={{ display: "flex", alignItems: "center" }}>
+                <Typography
+                  variant="subtitle2"
+                  gutterBottom
+                  sx={{ fontWeight: 100, fontSize: 10 }}
+                >
+                  Gender
+                </Typography>
+              </Grid>
+              <Grid item xs={10} sx={{ display: "flex", alignItems: "center" }}>
+                <FormControl>
+                  <RadioGroup
+                    row
+                    aria-labelledby="demo-controlled-radio-buttons-group"
+                    name="controlled-radio-buttons-group"
+                    value={targetGender}
+                    onChange={(e) => setTargetGender(e.target.value)}
+                    size="small"
+                    sx={{ m: 0 }}
+                  >
+                    <Box
+                      sx={{ m: 0 }}
+                      sx={{ display: "flex", alignItems: "center" }}
+                    >
+                      <FormControlLabel
+                        sx={{ m: 0 }}
+                        value="female"
+                        control={<Radio size="small" />}
+                        labelPlacement="start"
+                      />
 
-        <Button
-          sx={{ mt: 1 }}
-          variant="contained"
-          onClick={(e) => handleSubmit(e)}
-        >
-          Save
-        </Button>
-      </FormGroup>
+                      <Woman2OutlinedIcon sx={{ m: 0 }} />
+                    </Box>
+                    <Box
+                      sx={{ m: 0 }}
+                      sx={{ display: "flex", alignItems: "center" }}
+                    >
+                      <FormControlLabel
+                        sx={{ m: 0 }}
+                        value="male"
+                        control={<Radio size="small" />}
+                        labelPlacement="start"
+                      />
+
+                      <Man2OutlinedIcon sx={{ m: 0 }} />
+                    </Box>
+                    <Box
+                      sx={{ m: 0 }}
+                      sx={{ display: "flex", alignItems: "center" }}
+                    >
+                      <FormControlLabel
+                        sx={{ m: 0 }}
+                        value="all"
+                        control={<Radio size="small" />}
+                        labelPlacement="start"
+                      />
+                      <WcOutlinedIcon sx={{ m: 0 }} />
+                    </Box>
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
+              <Grid item xs={2} sx={{ display: "flex", alignItems: "center" }}>
+                <Typography
+                  variant="subtitle2"
+                  gutterBottom
+                  sx={{ fontWeight: 100, fontSize: 10 }}
+                >
+                  Age
+                </Typography>
+              </Grid>
+              <Grid item xs={10} sx={{ display: "flex", alignItems: "center" }}>
+                <FormControl sx={{ width: "100%" }}>
+                  {" "}
+                  <Slider
+                    getAriaLabel={() => "Minimum distance"}
+                    value={targetAgeRange}
+                    onChange={handleChange}
+                    valueLabelDisplay="auto"
+                    disableSwap
+
+                    // marks={marks}
+                    // valueLabelDisplay="on"
+                  />
+                </FormControl>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grid>
+        <Grid item xs={12}>
+          {" "}
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl>
+            <FormControlLabel
+              control={
+                <Switch checked={repetition} onChange={handleRepeatChange} />
+              }
+              label={repetition ? "Recurrent Event" : "Single Event"}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          {!repetition && (
+            <FormControl label="Responsive variant">
+              <DatePicker
+                label="Event Date"
+                value={date}
+                onChange={(newValue) => setDate(newValue)}
+              />
+            </FormControl>
+          )}
+        </Grid>
+
+        <Grid item xs={12}>
+          <Button
+            sx={{ mt: 1 }}
+            variant="contained"
+            onClick={(e) => handleSubmit(e)}
+          >
+            Save
+          </Button>
+        </Grid>
+      </Grid>
     </>
   );
 };
