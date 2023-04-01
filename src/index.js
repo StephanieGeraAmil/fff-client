f9d322c59237ab3ffc096c566717683c5716af27
Stephanie <stephanie.gera.amil@gmail.com>
2023-04-01 13:51:41 -0300

    more trying

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./reducers/reducers.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+const { REDIRECT_URI } = process.env;
+
 const store = createStore(
   reducers,
   composeWithDevTools(applyMiddleware(thunk))
