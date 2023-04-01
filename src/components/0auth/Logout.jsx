e0fc53e3d5cb70fdf55aeef52eaa500c6e1505cd
Stephanie <stephanie.gera.amil@gmail.com>
2023-04-01 13:51:41 -0300

    more trying

--- src/components/0auth/Logout.jsx
+++ src/components/0auth/Logout.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { LogoutOutlined } from "@mui/icons-material";
 import { IconButton } from "@mui/material/";
-
+const { REDIRECT_URI } = process.env;
 export const Logout = () => {
   const { logout } = useAuth0();
   return (
